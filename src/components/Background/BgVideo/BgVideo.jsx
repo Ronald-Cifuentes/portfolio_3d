77599d72cb062385d52fefda238aff3a9caa1d7b
Ronald Cifuentes <ronald.cifuentes@externos-cl.cencosud.com>
2023-08-19 19:27:04 -0500

    ✨ Feat: update bg video and description

--- src/components/Background/BgVideo/BgVideo.jsx
+++ src/components/Background/BgVideo/BgVideo.jsx
@@ -6,11 +6,14 @@ const BgVideo = () => {
   const attemptPlay = () => {
     video1El &&
       video1El.current &&
-      video1El.current.play().then(_ => {
-        console.log('Success video background')
-      }).catch(error => {
-        console.error('Error attempting to play', error)
-      })
+      video1El.current
+        .play()
+        .then(_ => {
+          console.log('Success video background')
+        })
+        .catch(error => {
+          console.error('Error attempting to play', error)
+        })
   }
 
   useEffect(() => {
@@ -19,7 +22,7 @@ const BgVideo = () => {
 
   return (
     <video className='absolute z-0' loop muted ref={video1El}>
-      <source src='https://cdn-r.s3.us-east-2.amazonaws.com/Abstract_Liquids.mkv' type='video/mp4' />
+      <source src='https://r-sources.ddns.net/Abstract_Liquids.mkv' type='video/mp4' />
       Your browser does not support HTML video.
     </video>
   )
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -8,21 +8,9 @@ import { fadeIn, textVariant } from '../../utils/motion'
 import './Skills.css'
 
 const Text = [
-  `I am an experienced software developer with knowledge in different Backend and Frontend
-technologies. I have skills in designing UX/UI, using version control, creating responsive
-layouts, and working with different editors and shortcuts. I also have knowledge of prompt
-engineering, which is the process of creating effective prompts for artificial intelligence
-models.`,
-  `On the backend side, I can develop applications using JavaScript, C, C++, C#, Java, Python,
-and PHP. I am familiar with different frameworks, libraries, and tools for each language. I
-can also work with various data bases, such as SQLServer, MongoDB, Firebase, and others. I know
-how to deploy and manage applications on cloud platforms, such as Azure, AWS, and Google
-Cloud. I also follow best practices for testing and security, such as unit testing,
-integration testing, code analysis, encryption, authentication, and authorization.`,
-  `I am passionate about learning new technologies and solving challenging problems. I have a
-strong work ethic and a collaborative attitude. I am always looking for opportunities to
-improve my skills and contribute to the success of the projects I work on. Let's work
-together to bring your ideas to life!`,
+  `I am an experienced software developer with knowledge in different Backend and Frontend technologies, such as React, Angular, Vue and Svelte. I have skills for UX/UI design, using version control, creating responsive designs and working with different editors and shortcuts. I also have knowledge of Prompt Engineering which is the process of creating effective Prompts for artificial intelligence models.`,
+  `On the backend side, I can develop applications using JavaScript, C, C++, C#, Java, Python and PHP. I am familiar with different frameworks, libraries and tools for each language. I can also work with various databases, such as SQLServer, MongoDB, Firebase and others. I know how to implement and manage applications on cloud platforms, such as Azure, AWS and Google Cloud. I also follow best practices for testing and security, such as unit testing, integration testing, code analysis, encryption, authentication and authorization.`,
+  `I am passionate about learning new technologies and solving challenging problems. I have a strong work ethic and a collaborative attitude. I am always looking for opportunities to improve my skills and contribute to the success of the projects I work on. Let's work together to bring your ideas to life!`,
 ]
 
 const Skills = () => {
